4f552f8ae010f8a35acc3ff7b0c80c3e00ad4073
Cosmin Dumitru <cosu@cosu.ro>
2015-03-08 00:12:44 +0100

    working code for street, price, location and link

--- spy.py
+++ spy.py
@@ -1,5 +1,8 @@
+from scrapy.contrib.loader import ItemLoader
+from scrapy.contrib.loader.processor import MapCompose
+from scrapy.contrib.loader.processor import TakeFirst
 from scrapy.crawler import Crawler
-from scrapy.selector import HtmlXPathSelector
+from scrapy.selector import  Selector
 from scrapy import log
 from scrapy import Spider, Item, Field
 from scrapy.settings import Settings
@@ -9,8 +12,16 @@ from twisted.internet import reactor
 __author__ = 'cosmin'
 
 
+class AdvertisedLoader(ItemLoader):
+    default_input_processor = MapCompose(unicode.strip)
+    default_output_processor = TakeFirst()
+
 class AdvertisedItem(Item):
+    price = Field()
     street = Field()
+    place = Field()
+    link = Field()
+
 
 
 class HuurSpider(Spider):
@@ -18,20 +29,22 @@ class HuurSpider(Spider):
 
     def parse(self, response):
 
-        item = AdvertisedItem()
+        selector = Selector(response)
+        for listed_ad in selector.xpath("//div[@class='woning']"):
+            l = ItemLoader(item=AdvertisedItem(), selector=listed_ad)
+            l.add_xpath("link", ".//a[@class='detaillink']/@href")
+            l.add_xpath("price", ".//span[@class='prijs right']")
+            l.add_xpath("place", ".//span[@class='plaats']")
+            l.add_xpath("street", ".//span[@class='straat']")
 
-        hxs = HtmlXPathSelector(response)
-
-        item['street'] = hxs.select("//span[@class='straat']/text()").extract()[0]
-
-        return item
-
+            yield l.load_item()
 
 
 
 
 
 if __name__ == '__main__':
+
     spider = HuurSpider()
     crawler = Crawler(Settings())
     crawler.configure()
