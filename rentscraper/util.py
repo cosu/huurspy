cba3c4a6092aa86e3ee480aa3002177823b12291
Cosmin Dumitru <cosu@cosu.ro>
2015-03-13 00:20:16 +0100

    whitespace util

--- rentscraper/util.py
+++ rentscraper/util.py
@@ -1,9 +1,14 @@
+import re
+
 __author__ = 'cdumitru'
 
 
 def remove_dot(string):
     return string.replace(".", "")
 
+
+def collapse_whitespace(string):
+    return re.sub('\s+', ' ', string)
 
 def has_pp(string):
     if "PP" in string:
