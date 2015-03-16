2683f29031340e088caca560c69047ee5453d091
Cosmin Dumitru <cosu@cosu.ro>
2015-03-16 23:10:04 +0100

    add funda distance

--- rentscraper/spiders/funda.py
+++ rentscraper/spiders/funda.py
@@ -89,7 +89,7 @@ class FundaLoader(ItemLoader):
 
 
 class FundaSpider(CrawlSpider):
-    name, start_urls = 'funda', ['http://www.funda.nl/huur/%s/sorteer-datum-af/p1']
+    name, start_urls = 'funda', ['http://www.funda.nl/huur/%s/+15km/sorteer-datum-af/p1']
     rules = (
         Rule(LxmlLinkExtractor(allow=('http://www.funda.nl/huur/'), restrict_xpaths=('//div[@id="pagerContainer"]')), callback='parse_page',
              follow=True),)
